2bdc892a8b136e932b1be1c3f5db8e93b586ac0f
Wendy.Arcila <wendy.arcilar@gmail.com>
2022-10-28 17:54:53 -0500

    creación del proyecto: primer avance, creación de la clase song

---
+++ src/main/java/Song.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Song {
+    private String name;
+    private Integer id;
+    private Date date;
+    private Double duration;
+    private  String cover;
+    private  String description;
+
+}
